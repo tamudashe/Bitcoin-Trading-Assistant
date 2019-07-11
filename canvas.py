34f665ebf20e1dee856f46dfc2d4eec9710f33c4
tamudashe <tamudashe@gmail.com>
2019-07-10 22:53:56 -0400

    Bitcoin USD app

--- canvas.py
+++ canvas.py
@@ -28,11 +28,23 @@ def my_webhook(event, value):
     # inserts our desired event
     idtt_event_url = my_webhook_url.format(event)
 
-    #sends a HTTP POST request to the webhook URL
+    # sends a HTTP POST request to the webhook URL
     requests.post(my_webhook_url, json=data)
 
 
 my_bit_threshold = 10000
+
+
+def format_bit_history(bit_history):
+    rows = []
+    # Formats the date into a string: '02.10.2019 10:41'
+    for bit_price in bit_history:
+        date = bit_price['date'].strftime("%m.%d.%Y %H:%M")
+        price = bit_price['price']
+        row = '{}: $<b>{}</b>'.format(date, price)
+        rows.append(row)
+
+        return '<br>'.join(rows)
 
 
 def main():
@@ -40,14 +52,24 @@ def main():
     while True:
         price = get_latest_bitcoin_price()
         date = datetime.now()
+        bit_history.append({'date': date, 'price': price})
+
+        # Send an emergency notification
+        if price < my_bit_threshold:
+            my_webhook('emergency_bitcoin_price', price)
+
+        # Send a Telegram notification
+        if len(bit_history) == 10:
+            my_webhook('bitcoin_update', format_bit_history(bit_history))
+
+            # Reset history
+            bit_history = []
+
+        # Sleep for 5 minutes
+        time.sleep(5 * 60)
 
 
-
-
-
-
-
-def if __name__ == '__main__':
+if __name__ == '__main__':
     main()
 
 
